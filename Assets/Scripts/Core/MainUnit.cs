5057ac693e0ccd8aaa86a2b3adc8710a0d94e472
Ernest <ernesto170696@gmail.com>
2021-11-25 13:40:04 +0300

    Aded Lessen2

---
+++ Assets/Scripts/Core/MainUnit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUnit : MonoBehaviour, ISelectable
+{
+    public float Health => _health;
+
+    public float MaxHealth => _maxHealth;
+
+    public Sprite Icon => _icon;
+
+    [SerializeField]
+    private float _maxHealth = 100;
+    [SerializeField]
+    private Sprite _icon;
+    private float _health;
+}
