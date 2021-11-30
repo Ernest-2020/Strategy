32a62f43545c9dbbad39094763feaa04c5f6375c
Ernest-2020 <73068574+Ernest-2020@users.noreply.github.com>
2021-11-30 16:41:04 +0300

    .

--- Assets/Scripts/UserControlSystem/UI/Model/UiModelInstaller.cs
+++ Assets/Scripts/UserControlSystem/UI/Model/UiModelInstaller.cs
@@ -1,18 +1,11 @@
-
-using UnityEngine;
 using Zenject;
 
 public class UIModelInstaller : MonoInstaller
 {
-    //[SerializeField] private AssetsContext _legacyContext;
-    //[SerializeField] private Vector3Value _vector3Value;
-    //[SerializeField] private AttackableValue _attackableValue;
-    //[SerializeField] private SelectableValue _selectableValue;
-
     public override void InstallBindings()
     {
         Container.Bind<CommandCreatorBase<IProduceUnitCommand>>()
-                .To<ProduceUnitCommandCommandCreator>().AsTransient();
+            .To<ProduceUnitCommandCommandCreator>().AsTransient();
         Container.Bind<CommandCreatorBase<IAttackCommand>>()
             .To<AttackCommandCommandCreator>().AsTransient();
         Container.Bind<CommandCreatorBase<IMoveCommand>>()
