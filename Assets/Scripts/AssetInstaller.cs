91b8de7fc11c450848f4017bd2b9b6657f42d781
Ernest-2020 <73068574+Ernest-2020@users.noreply.github.com>
2021-12-01 17:12:16 +0300

    Lesson4

--- Assets/Scripts/AssetInstaller.cs
+++ Assets/Scripts/AssetInstaller.cs
@@ -10,7 +10,10 @@ public class AssetInstaller : ScriptableObjectInstaller<AssetInstaller>
     [SerializeField] private SelectableValue _selectables;
     public override void InstallBindings()
     {
-        Container.BindInstances(_legacyContext, _groundClicksRMB, _attackableClicksRMB, _selectables);
-
+        Container.Bind<IAwaitable<IAttackable>>()
+            .FromInstance(_attackableClicksRMB);
+        Container.Bind<IAwaitable<Vector3>>()
+            .FromInstance(_groundClicksRMB);
+        Container.BindInstances(_legacyContext, _selectables);
     }
 }
--- Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveCommandExecutor : CommandExecutorBase<IMoveCommand>
 {
-	public override void ExecuteSpecificCommand(IMoveCommand command)
+	[SerializeField] private UnitMovementStop _stop;
+	[SerializeField] private Animator _animator;
+	public override async void ExecuteSpecificCommand(IMoveCommand command)
 	{
-		Debug.Log($"{name} is moving to {command.Target}!");
+		GetComponent<NavMeshAgent>().destination = command.Target;
+		_animator.SetTrigger("Walk");
+		await _stop;
+		_animator.SetTrigger("Idle");
+
 	}
 }
